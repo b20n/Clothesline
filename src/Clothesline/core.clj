b28e6f883bc3d2449ec45df56a4f46601b251aed
Dave Fayram <dfayram@gmail.com>
2010-09-27 13:36:59 -0700

    Lein startup

---
+++ src/Clothesline/core.clj
@@ -0,0 +1,1 @@
+(ns Clothesline.core)
---
+++ test/Clothesline/test/core.clj
@@ -0,0 +1,6 @@
+(ns Clothesline.test.core
+  (:use [Clothesline.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
