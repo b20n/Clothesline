016b2257aa369d3579c8d4f70630bf0ef2321396
Dave Fayram <dfayram@gmail.com>
2010-09-27 13:43:18 -0700

    We'll use ring as a basis

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject Clothesline "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [ring "0.3.1"]]
+  :dev-dependencies [[swank-clojure "1.2.0"]])
