476e99035695b717ed8ba9590be9fda122951fdd
Dave Fayram <dfayram@gmail.com>
2010-10-01 12:48:15 -0700

    More updates to graph and syntax.

--- src/Clothesline/protocol/graph.clj
+++ src/Clothesline/protocol/graph.clj
@@ -1,14 +1,61 @@
 (ns clothesline.protocol.graph
   (:use [clothesline.protocol [response-helpers]
          [syntax]])
-  (:require [clothesline [service :as handler]]))
+  (:require [clothesline [service :as s]]))
 
 ;; Proposed syntax
 
 (defstate b13
-  :haltable true
-  :test (fn [handler request]
-          (handler/service-available? handler request))
-  :success :b12
-  :fail (stop-response 503))
+  "Check if service has been made unavailable. Return status 503 if not."
+  :test s/service-available?
+  :yes :b12
+  :no (stop-response 503))
+
+(defstate b12
+  :test (constantly true)
+  :yes :b11
+  :no (stop-response 501))
+
+(defstate b11
+  :test s/uri-too-long?
+  :no :b10
+  :yes (stop-response 414))
+
+(defstate b10
+  :test (fn [handler request] ((s/allowed-methods handler request)
+                               (:request-method request)) )
+  :yes :b11
+  :no (stop-response 501))
+
+(defstate b9
+  :test s/malformed-request?
+  :no :b8
+  :yes (stop-response 400))
+
+(defstate b8
+  :test s/authorized?
+  :yes :b7
+  :no (stop-response 401))
+
+(defstate b7
+  :test s/forbidden?
+  :yes (stop-response 403)
+  :no  :b6)
+
+(defstate b6
+  :test s/valid-content-headers?
+  :yes :b5
+  :no (stop-response 501))
+
+(defstate b5
+  :test s/known-content-type?
+  :yes :b4
+  :no (stop-response 415))
+
+(defstate b4
+  :test s/valid-entity-length?
+  :yes :b3
+  :no (stop-response 413))
+
+
 
--- src/Clothesline/protocol/syntax.clj
+++ src/Clothesline/protocol/syntax.clj
@@ -3,42 +3,50 @@
         [clothesline.protocol [response-helpers]]))
 
 (defmacro defstate [name & forms]
-  `(def ~name (state ~@forms)))
+  (let [docstring (reduce str (interpose "\n" (take-while string? forms)))
+        rforms    (drop-while string? forms)
+        mname     (with-meta name {:doc docstring})]
+    `(def ~mname (state ~@rforms))))
 
 (def state-standards
      {:haltable true
-      :test (fn [_ _] false)
-      :fail (stop-response 500)
+      :test (fn [& _] false)
+      :no (stop-response 500)
+      :yes (stop-response 500)
       })
 
 (def *graph-namespace* 'clothesline.protocol.graph)
 (defn- key->sym [keyword] (symbol (name keyword)))
+(defn- key->nsvar [ns keyword] (if (keyword? keyword)
+                                 (or (ns-resolve ns (key->sym keyword)) keyword)
+                                 keyword))
 
-(defn- resolve-states [map]
-  (update-in map [:pass :fail]
-             (fn [v] (if (keyword? v)
-                       (ns-resolve *graph-namespace* (key->sym v))
-                       v))))
+
+(defn resolve-states [map] 
+  (assoc map :yes (key->nsvar *graph-namespace* (:yes map))
+             :no  (key->nsvar *graph-namespace* (:no map))))
+  
 
 (defmacro state [& forms]
   (let [state-opts (apply hash-map forms)
         opts (resolve-states (merge state-standards state-opts))
         has-body? (:body opts)]
     (if-not has-body?
-      `(fn [[handler# request# :as args#]]
-         (let [test# ~(:test opts)]
-           (let [result# (test# handler# request#)
-                 plan#   (if result#
-                           ~(:success opts)
-                           ~(:fail opts))]
-             (println "Intermediate: " result# " " plan#)
-             (cond
-              (var? plan#) (apply plan# args#)
-              (map? result#) result#    ;TODO: Consider if this is a bad idea or not.
-              (map? plan#)   plan#
-              (instance? clojure.lang.IFn plan#) (apply plan# args#)
-              :default plan#)
-             )))
+      `(fn [& [handler# request# :as args#]]
+         (let [test# ~(:test opts)
+               result# (test# handler# request#)
+               plan#   (if result#
+                         ~(:yes opts)
+                         ~(:no opts))]
+           (println "Intermediate: " result# " " plan#)
+           (cond
+            (var? plan#) (apply plan# args#)
+            (map? result#) result# ;TODO: Consider if this is a bad idea or not.
+            (map? plan#)   plan#
+            (keyword? plan#) (list result# plan#)
+            (instance? clojure.lang.IFn plan#) (apply plan# args#)
+            :default plan#)
+           ))
       (throw (Exception. "Not ready yet!")))))
 
 
