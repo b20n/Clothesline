ca8ba0b1eeaa0c75985f08a7bbeff2fba52ca494
Dave Fayram <dfayram@gmail.com>
2010-10-01 16:01:23 -0700

    State threading. Handlers can now return bools or hashes.

--- src/Clothesline/protocol/syntax.clj
+++ src/Clothesline/protocol/syntax.clj
@@ -5,7 +5,8 @@
 (defmacro defstate [name & forms]
   (let [docstring (reduce str (interpose "\n" (take-while string? forms)))
         rforms    (drop-while string? forms)
-        mname     (with-meta name {:doc docstring})]
+        mname     (with-meta name {:doc docstring :name name
+                                   })]
     `(def ~mname (state ~@rforms))))
 
 (def state-standards
@@ -26,25 +27,43 @@
   (assoc map :yes (key->nsvar *graph-namespace* (:yes map))
              :no  (key->nsvar *graph-namespace* (:no map))))
   
+(defn update-response [{:keys [add-headers remove-headers headers body status] :as map}
+                       response]
+  (-> response
+      (assoc :headers (or headers (:headers response)))
+      (update-in [:headers] #(merge % (or add-headers {})))
+      (update-in [:headers] #(apply dissoc (list* % (or remove-headers []))))
+      (assoc :body (or (:body response) body))
+      (assoc :status (or (:status response) status))))
 
 (defmacro state [& forms]
   (let [state-opts (apply hash-map forms)
         opts (resolve-states (merge state-standards state-opts))
         has-body? (:body opts)]
     (if-not has-body?
-      `(fn [& [handler# request# :as args#]]
+      `(fn [& [ {request# :request
+                 handler# :handler
+                 response# :response :as args#}]]
          (let [test# ~(:test opts)
-               result# (test# handler# request#)
+               test-result# (test# handler# request#)
+               result# (or (:result test-result#)
+                           test-result#)
                plan#   (if result#
                          ~(:yes opts)
-                         ~(:no opts))]
-           (println "Intermediate: " result# " " plan#)
+                         ~(:no opts))
+               nresponse# (if (map? test-result#)
+                            (update-response test-result# response#)
+                            response#)
+               forward-args# {:request request#
+                              :handler handler#
+                              :response nresponse#}]
+           (println "Intermediate: " test-result# " -> " result#) ; For debugging
            (cond
-            (var? plan#) (apply plan# args#)
-            (map? result#) result# ;TODO: Consider if this is a bad idea or not.
             (map? plan#)   plan#
+            (instance? clojure.lang.IFn plan#) (apply plan# (list args#))
+            (var? plan#) (apply plan# (list forward-args#))
             (keyword? plan#) (list result# plan#)
-            (instance? clojure.lang.IFn plan#) (apply plan# args#)
+
             :default plan#)
            ))
       (throw (Exception. "Not ready yet!")))))
