5b4d47f9639ae699d58256a0638be24708049181
Dave Fayram <dfayram@gmail.com>
2010-10-07 16:35:20 -0700

    Graph helpers added.

---
+++ src/Clothesline/protocol/graph_helpers.clj
@@ -0,0 +1,37 @@
+(ns clothesline.protocol.graph-helpers
+  (:use (clothesline.protocol.response-helpers)))
+
+;; Syntax helpers
+
+(defmacro request-header-exists
+  [header-name]
+  `(fn [{{headers# :headers} :request}]
+    (if (get headers# ~header-name)
+      true
+      false)))
+
+(defmacro graphdata-item-exists
+  [kwd-name]
+  `(fn [{graphdata# :graphdata}]
+     (contains? graphdata# ~kwd-name)))
+
+(defmacro request-header-is [header-name value-str]
+  `(fn [{{headers# :headers} :request}]
+     (let [hv# (get headers# ~header-name)]
+       (= hv# ~value-str))))
+
+(defmacro is-request-method [req-symbol]
+  `(fn [{{request-method# :request-method} :request}]
+     (= ~req-symbol request-method#)))
+
+(defmacro call-on-handler [protocol-method]
+  `(fn [{handler# :handler
+         request# :request
+         data#    :graphdata}]
+     (apply ~protocol-method (list handler# request# data#))))
+
+
+
+
+
+
