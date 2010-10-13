f97b906ea6e895bec13e07e8c63878fbb251bd62
Dave Fayram <dfayram@gmail.com>
2010-10-13 14:56:08 -0700

    Adding clj-stacktrace for debug help.

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [ring "0.3.1"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
+                     [clj-stacktrace "0.2.0"]])
