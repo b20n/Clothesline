9d39922106e3f19ec0acaf8a33e89fe922e39081
Thomas Lockney <thomas@lockney.net>
2010-10-13 17:19:10 -0700

    Adding in very basic Jetty request handler

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
+                 [clout "0.3.1"]
                  [ring "0.3.1"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
---
+++ src/Clothesline/request_handler.clj
@@ -0,0 +1,21 @@
+(ns clothesline.request_handler
+  (:use ring.util.response
+        ring.adapter.jetty
+        clout.core)
+  (:require [clothesline.protocol (graph :as g)]))
+
+(defn no-handler-found [req graph-data]
+  "Returns a 404 when no appropriate handler was found"
+  (-> (response "Not found")
+      (status 404)))
+
+(defn get-route
+  "Selects the appropriate place to route the request based on the supplied map"
+  ([route-map req default]
+    (let [[route fun] (first (filter #(route-matches (first %) req) route-map))]
+      (or fun default)))
+  ([route-map req] (get-route route-map req no-handler-found)))
+
+(defn handler [req]
+  "Slim little shim for getting the route and doing something with it"
+  (g/start (get-route *routes* req) req {}))
