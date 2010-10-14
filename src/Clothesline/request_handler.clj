3d3e7f346cce267bcb8897b7e355e3b155f4cb8b
Thomas Lockney <thomas@lockney.net>
2010-10-14 11:39:24 -0700

    Made routes a global and added back in wrap-params call

--- src/Clothesline/request_handler.clj
+++ src/Clothesline/request_handler.clj
@@ -1,15 +1,15 @@
 (ns clothesline.request-handler
   (:use ring.util.response
-        ring.adapter.jetty
+        ring.middleware.params
         clout.core
         [clothesline.protocol (graph :as g)]))
 
-(defonce routes (ref {}))
+(defonce *routes* (ref {}))
 
 (defn set-routes [route-map] 
   "Set the route map to be used by the handler. This should take the form of
    a URL path as the key and a ref to a service implementation as the value"
-  (dosync (alter routes merge route-map)))
+  (dosync (alter *routes* merge route-map)))
 
 (defn no-handler-found [req]
   "Returns a 404 when no appropriate handler was found"
@@ -22,12 +22,12 @@
 
 (defn handler [req]
   "Slim little shim for getting the route and doing something with it"
-  (let [route (get-route @routes req)]
+  (let [route (get-route @*routes* req)]
     ;; er, I think (from this gist https://gist.github.com/3085f7636f6be32b2ef4)
     ;; that this is how it should actually be called. Yes?
     (if route 
-      (g/start {:handler route 
-                :request req 
-               :graphdata {}})
+      (wrap-params (g/start {:handler route 
+                            :request req 
+                            :graphdata {}}))
       (no-handler-found req))))
 
