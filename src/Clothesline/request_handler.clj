8dd898407dd8959fb69816d351984155836f24b0
Dave Fayram <dfayram@gmail.com>
2010-10-14 11:17:08 -0700

    More idiomatic route storage.

--- src/Clothesline/request_handler.clj
+++ src/Clothesline/request_handler.clj
@@ -4,10 +4,12 @@
         clout.core
         [clothesline.protocol (graph :as g)]))
 
+(defonce routes (ref {}))
+
 (defn set-routes [route-map] 
   "Set the route map to be used by the handler. This should take the form of
    a URL path as the key and a ref to a service implementation as the value"
-  (def routes route-map))
+  (dosync (alter routes merge route-map)))
 
 (defn no-handler-found [req]
   "Returns a 404 when no appropriate handler was found"
@@ -20,7 +22,7 @@
 
 (defn handler [req]
   "Slim little shim for getting the route and doing something with it"
-  (let [route (get-route routes req)]
+  (let [route (get-route @routes req)]
     ;; er, I think (from this gist https://gist.github.com/3085f7636f6be32b2ef4)
     ;; that this is how it should actually be called. Yes?
     (if route 
