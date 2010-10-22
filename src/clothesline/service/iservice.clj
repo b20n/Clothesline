eea5b7b7b333e89b611b5ee24ca88fae15dffaf9
Dave Fayram <dfayram@gmail.com>
2010-10-22 12:01:39 -0700

    Fixing method signature for IService.

--- src/clothesline/service/iservice.clj
+++ src/clothesline/service/iservice.clj
@@ -2,32 +2,61 @@
 (gen-interface
   :name clothesline.service.IService
   :methods [
-            [resourceExists       [clojure.lang.APersistentMap] boolean]
-            [serviceAvailable     [clojure.lang.APersistentMap] boolean]
-            [authorized           [clojure.lang.APersistentMap] boolean]
-            [forbidden            [clojure.lang.APersistentMap] boolean]
-            [allowMissingPost     [clojure.lang.APersistentMap] boolean]
-            [malformedRequest     [clojure.lang.APersistentMap] boolean]
-            [uriTooLong           [clojure.lang.APersistentMap] boolean]
-            [knownContentType     [clojure.lang.APersistentMap] boolean]
-            [validContentHeaders  [clojure.lang.APersistentMap] boolean]
-            [options              [clojure.lang.APersistentMap] clojure.lang.APersistentMap]
-            [allowedMethods       [clojure.lang.APersistentMap] clojure.lang.APersistentMap]
-            [deleteResource       [clojure.lang.APersistentMap] boolean]
-            [deleteCompleted      [clojure.lang.APersistentMap] boolean]
-            [postIsCreate         [clojure.lang.APersistentMap] boolean]
-            [createPath           [clojure.lang.APersistentMap] String]
-            [processPost          [clojure.lang.APersistentMap] boolean]
-            [contentTypesProvided [clojure.lang.APersistentMap] clojure.lang.APersistentMap]
-            [contentTypesAccepted [clojure.lang.APersistentMap] clojure.lang.APersistentMap]
-            [charsetsProvided     [clojure.lang.APersistentMap] clojure.lang.APersistentMap]
-            [encodingsProvided    [clojure.lang.APersistentMap] clojure.lang.APersistentMap]
-            [variances            [clojure.lang.APersistentMap] clojure.lang.APersistentMap]
-            [conflict             [clojure.lang.APersistentMap] boolean]
-            [multipleChoices      [clojure.lang.APersistentMap] boolean]
-            [previouslyExisted    [clojure.lang.APersistentMap] boolean]
-            [movedPermantently    [clojure.lang.APersistentMap] boolean]
-            [lastModified         [clojure.lang.APersistentMap] java.util.Date]
-            [expires              [clojure.lang.APersistentMap] java.util.Date]
-            [generateETag         [clojure.lang.APersistentMap] String]
-            [finishRquest         [clojure.lang.APersistentMap] boolean]])
+            [resourceExists
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] boolean]
+            [serviceAvailable
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] boolean]
+            [authorized
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] boolean]
+            [forbidden
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] boolean]
+            [allowMissingPost
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] boolean]
+            [malformedRequest
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] boolean]
+            [uriTooLong
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] boolean]
+            [knownContentType
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] boolean]
+            [validContentHeaders
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] boolean]
+            [options
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] clojure.lang.APersistentMap]
+            [allowedMethods
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] clojure.lang.APersistentMap]
+            [deleteResource
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] boolean]
+            [deleteCompleted
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] boolean]
+            [postIsCreate
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] boolean]
+            [createPath
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] String]
+            [processPost
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] boolean]
+            [contentTypesProvided
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] clojure.lang.APersistentMap]
+            [contentTypesAccepted
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] clojure.lang.APersistentMap]
+            [charsetsProvided
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] clojure.lang.APersistentMap]
+            [encodingsProvided
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] clojure.lang.APersistentMap]
+            [variances
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] clojure.lang.APersistentMap]
+            [conflict
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] boolean]
+            [multipleChoices
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] boolean]
+            [previouslyExisted
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] boolean]
+            [movedPermantently
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] boolean]
+            [lastModified
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] java.util.Date]
+            [expires
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] java.util.Date]
+            [generateETag
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] String]
+            [finishRquest
+             [clothesline.service.IService clojure.lang.APersistentMap clojure.lang.APersistentMap] boolean]])
