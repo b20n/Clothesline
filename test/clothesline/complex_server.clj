d57db3257b32291b134fbd7e7b1072c314af0596
Daniel Pittman <daniel@rimspace.net>
2011-09-06 23:45:54 -0700

    Add `defhandler` to underlay `defsimplehandler`...
    `defhandler` wraps the complexity of building a handler, together with default
    behaviour, in a simple macro that expands, more or less, to define a type,
    augment the default behaviours with the supplied behaviours, and then
    creates a ready-to-use instance for the user.
    
    `defsimplehandler` is reimplemented in terms of `defhandler`, and the complex
    server test is also implemented using the wrapper macro.
    
    Finally, update the documentation to reflect these changes.
    
    Signed-off-by: Daniel Pittman <daniel@rimspace.net>

--- test/clothesline/complex_server.clj
+++ test/clothesline/complex_server.clj
@@ -14,49 +14,39 @@
 
 
 ;; Behavior
-(def behavior
-     {:allowed-methods (fn [_ _ _]
-                         (test/annotated-return #{:get :post :put}
-                                                {:annotate {:debug-output
-                                                            (fn [_] (println "o/~"))}}))
-      
-      :malformed-request? (fn [_ {:keys [request-method params]} _]
-                            (let [name (params "name")
-                                  location (params "location")]
-                              (cond
-                               (= request-method :get) (nil? name)
-                               :otherwise              (or (nil? name)
-                                                           (nil? location)))))
-      :previously-existed? (fn [_ {params :params} _] (get-name-url (params "name")))
-      :resource-exists? (constantly false)
-      :allow-missing-post? (constantly true)
-      :moved-permanently? (fn [_ {params :params method :request-method} _]
-                            
-                            (if (= method :put)
-                              false
-                              (get-name-url (params "name"))))
-      :post-is-create? (fn [_ {params :params} _] (not (name-exists? (params "name"))))
-      :create-path (fn [_ {{:strs [name]} :params} _] (test/annotated-return (str "/" name)))
-      :process-post (fn [_ {{:strs [name location]} :params :as request} _]
-                         (add-name-url name location)
-                         (test/annotated-return true))
+(helpers/defhandler bookmark-handler
+  :allowed-methods (fn [_ _ _]
+                     (test/annotated-return #{:get :post :put}
+                                            {:annotate {:debug-output
+                                                        (fn [_] (println "o/~"))}}))
+  :malformed-request? (fn [_ {:keys [request-method params]} _]
+                        (let [name (params "name")
+                              location (params "location")]
+                          (cond
+                           (= request-method :get) (nil? name)
+                           :otherwise              (or (nil? name)
+                                                       (nil? location)))))
+  :previously-existed? (fn [_ {params :params} _] (get-name-url (params "name")))
+  :resource-exists?    (constantly false)
+  :allow-missing-post? (constantly true)
+  :moved-permanently?  (fn [_ {params :params method :request-method} _]
+                         (if (= method :put)
+                           false
+                           (get-name-url (params "name"))))
+  :post-is-create? (fn [_ {params :params} _] (not (name-exists? (params "name"))))
+  :create-path (fn [_ {{:strs [name]} :params} _] (test/annotated-return (str "/" name)))
+  :process-post (fn [_ {{:strs [name location]} :params :as request} _]
+                  (add-name-url name location)
+                  (test/annotated-return true))
 
-      ;; This is mostly just to handle params
-      :content-types-accepted (fn [& _]
-                                {"*/*" (fn [{params :params body :body} _]
-                                         (add-name-url (params "name")
-                                                       (params "location")))})
-     }
-
-)
-
-(defrecord bookmark-handler [])
-(helpers/extend-as-handler bookmark-handler behavior)
+  ;; This is mostly just to handle params
+  :content-types-accepted (fn [& _]
+                            {"*/*" (fn [{params :params body :body} _]
+                                     (add-name-url (params "name")
+                                                   (params "location")))}))
 
 ;; Server
-
-
-(defonce *server* (delay (clothesline.core/produce-server {"/:name" (bookmark-handler.)} 
-                                                          {:join? false :port 9001})))
-
-
+(defonce *server*
+  (delay (clothesline.core/produce-server
+          {"/:name" bookmark-handler}
+          {:join? false :port 9001})))
